1bd4a9930da74448ea4bcc019129b7cb935cc1ea
kimdozzi <neo5188@gmail.com>
2023-12-23 15:09:44 +0900

    first commit

---
+++ src/test/java/com/ll/reviewsite/ReviewsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.reviewsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
